df302d8891765879f5127b61b4aaa19cce2eb3cc
yasoon13 <49785758+yasoon13@users.noreply.github.com>
2023-04-12 16:15:12 +0500

    added new feature

--- greetings.js
+++ greetings.js
@@ -1,3 +1,5 @@
 // this is a javascript file
 var greetings = "Hello World";
 console.log(greetings);
+var makeChanges = "This will initiate a pull request automatically";
+console.log("makeChanges");
